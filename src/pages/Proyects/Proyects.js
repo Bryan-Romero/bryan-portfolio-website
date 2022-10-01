7985e709daa95de5367d79a4a1ebaf605868a68a
Bryan Romero <109822721+BryanRomeroEverst@users.noreply.github.com>
2022-10-01 13:35:09 -0600

    grid for list of proyect

--- src/pages/Proyects/Proyects.js
+++ src/pages/Proyects/Proyects.js
@@ -7,8 +7,36 @@ import ProyectSection from "../../components/ProyectSection/ProyectSection";
 const Proyects = () => {
     return(
         <div className="proyectsContainer">
-            <ProyectSection title={'Works'}>
+            <ProyectSection title={'Personales'}>
                 <Proyect 
+                    imgProyect={imgProfile}
+                    nameProyect={'Proyecto 1'}
+                    littleDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+                />
+                <Proyect 
+                    imgProyect={imgProfile}
+                    nameProyect={'Proyecto 2'}
+                    littleDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+                />
+                <Proyect 
+                    imgProyect={imgProfile}
+                    nameProyect={'Proyecto 3'}
+                    littleDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+                />
+                <Proyect 
+                    imgProyect={imgProfile}
+                    nameProyect={'Proyecto 4'}
+                    littleDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+                />
+                <Proyect 
+                    imgProyect={imgProfile}
+                    nameProyect={'Proyecto 5'}
+                    littleDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+                />
+            </ProyectSection>
+
+            <ProyectSection title={'Trabajo'}>
+            <Proyect 
                     imgProyect={imgProfile}
                     nameProyect={'Proyecto 1'}
                     littleDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
